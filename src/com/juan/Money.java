4e04da046a7ef0f0086fa18d629b38ba6afcf67f
juancarranza <2052891770114@ingenieria.usac.edu.gt>
2020-03-16 13:44:38 -0600

    Currency change

--- src/com/juan/Money.java
+++ src/com/juan/Money.java
@@ -1,10 +1,24 @@
 package com.juan;
 
+import sun.util.resources.cldr.yav.CurrencyNames_yav;
+
+enum Currency{
+    USD, EURO;
+    public double conversionRateTo(Currency target){return 1.0;}
+}
+
 public class Money {
 
     private double value;
-    public Money(double value){
+    private Currency currency;
+    public Money(double value, Currency currency){
         this.value=value;
+        this.currency=currency;
+
+    }
+
+    public Currency getCurrency(){
+        return this.currency;
     }
 
     public double getValue(){
@@ -21,10 +35,13 @@ class Test{
     private  static void dispenseFunds(Money amount){/**/}
     public static void test(){
 
-        Money balance = new Money(1.0);
-        Money request = new Money(3.0);
+        Money balance = new Money(1.0,Currency.EURO);
+        Money request = new Money(1.0,Currency.USD);
 
-        if (balance.getValue()>request.getValue()){
+        double normalizedBalance = balance.getValue()*balance.getCurrency().conversionRateTo(Currency.USD);
+        double normalizedRequest = request.getValue()*request.getCurrency().conversionRateTo(Currency.USD);
+
+        if(normalizedBalance>normalizedRequest){
             dispenseFunds(request);
         }
     }
